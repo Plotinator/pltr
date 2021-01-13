2fbf18d7f84dfb50b3539e7c3a7045b96b48146a
Cameron Sutter <cameronsutter0@gmail.com>
2021-01-13 14:09:09 -0700

    finish remove zoom

--- v2/migrator/migrations/2021.1.15.js
+++ v2/migrator/migrations/2021.1.15.js
@@ -1,12 +1,14 @@
 const { cloneDeep } = require('lodash')
 
 function migrate (data) {
-  if (data.file && data.file.version === '2020.11.19') return data
+  if (data.file && data.file.version === '2021.1.15') return data
 
   let obj = cloneDeep(data)
 
   delete obj.ui.zoomIndex
   delete obj.ui.zoomState
+
+  obj.ui.timeline = { size: 'large' }
 
   return obj
 }
