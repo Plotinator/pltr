43cbe2a6b0895a0a569de7a2cc44804698f7a6cb
Arian Hendricks <arian.hendricks@gmail.com>
2021-01-16 18:58:22 -0500

    Modify Rename and Trash buttons to accept and return data

--- src/components/ui/RenameButton.js
+++ src/components/ui/RenameButton.js
@@ -3,8 +3,11 @@ import { Button, Text } from 'native-base'
 import t from 'format-message'
 
 export default function RenameButton (props) {
-  const noop = () => {}
-  return <Button full onPress={props.onPress ?? noop} style={[{ flex: 1, width: 85 }, props.buttonStyle ?? {}]}>
+  const handlePress = () => {
+    const { onPress, data } = props
+    if(onPress) onPress(data)
+  }
+  return <Button full onPress={handlePress} style={[{ flex: 1, width: 85 }, props.buttonStyle ?? {}]}>
     <Text>{t('Rename')}</Text>
   </Button>
 }
--- src/components/ui/TrashButton.js
+++ src/components/ui/TrashButton.js
@@ -2,8 +2,11 @@ import React from 'react'
 import { Button, Icon } from 'native-base'
 
 export default function TrashButton (props) {
-  const noop = () => {}
-  return <Button full danger onPress={props.onPress ?? noop} style={[{ flex: 1, width: 75 }, props.buttonStyle ?? {}]}>
+  const handlePress = () => {
+    const { onPress, data } = props
+    if(onPress) onPress(data)
+  }
+  return <Button full danger onPress={handlePress} style={[{ flex: 1, width: 75 }, props.buttonStyle ?? {}]}>
     <Icon type='FontAwesome5' name='trash' style={props.iconStyle}/>
   </Button>
 }
