fcba8927941e1cc8ef2b820da8eadf0be5b6a2c3
Arian Hendricks <arian.hendricks@gmail.com>
2021-01-28 15:55:28 -0500

    Revised App's core User Verification algorhythm (part 1)

--- src/components/App.js
+++ src/components/App.js
@@ -1,351 +1,182 @@
-import React, { useEffect, useState } from 'react'
+import React, { Component, useEffect, useState } from 'react'
 import {
-  StyleSheet,
   View,
-  StatusBar,
-  Linking,
-  NativeModules,
-  Platform,
   Keyboard,
+  StatusBar,
   KeyboardAvoidingView,
   TouchableWithoutFeedback
 } from 'react-native'
-const { DocumentBrowser } = NativeModules
-import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context'
-import {
-  Content,
-  Text,
-  H1,
-  H2,
-  Form,
-  Item,
-  Input,
-  Button,
-  Label,
-  Spinner,
-  Toast,
-  Root,
-  Container
-} from 'native-base'
+import { SafeAreaProvider } from 'react-native-safe-area-context'
 import {
   checkForActiveLicense,
   getUserVerification,
+  checkStoredLicense,
   verifyUser,
-  reset,
-  checkStoredLicense
+  reset
 } from '../utils/user_info'
-import Main from './Main'
 import { sendVerificationEmail } from '../utils/api'
 import AppErrorBoundary from './AppErrorBoundary'
 import t from 'format-message'
-import { AlertDialog } from './shared/common'
+import AlertDialog, { showAlert } from './shared/common/AlertDialog'
 import Metrics from '../utils/Metrics'
-import { screenWidth, screenHeight } from '../utils/Metrics'
+import Main from './Main'
+import { cloneDeep } from 'lodash'
 
 const { IS_ANDROID } = Metrics
-const App = () => {
-  const [userInfo, setUserInfo] = useState(null)
-  const [email, setEmail] = useState('')
-  const [code, setCode] = useState('')
-  const [verifying, setVerifying] = useState(false)
 
-  useEffect(() => {
-    let ignore = false
-
-    async function fetchUserInfo () {
-      // DEV: use the following to reset user info/verification
-      // await reset()
-      const fetchedInfo = await getUserVerification()
-      // if (fetchedInfo) {
-      //   console.log('USER_INFO', fetchedInfo)
-      // }
-      if (!fetchedInfo) {
-        if (Platform.OS == 'ios') {
-          DocumentBrowser.closeBrowser()
-        } else if (Platform.OS == 'android') {
-          NativeModules.AndroidDocumentBrowser.closeBrowser()
-        }
-      }
-      if (!ignore) setUserInfo(fetchedInfo)
-    }
-
-    fetchUserInfo()
-    return () => (ignore = true)
-  }, [])
-
-  useEffect(
-    () => {
-      async function checkLicenseIsStillActive () {
-        const isActive = await checkStoredLicense()
-        if (!isActive) {
-          // TODO: show something if not active
-        }
-      }
-      if (userInfo && userInfo.verified && userInfo.validLicense) {
-        checkLicenseIsStillActive()
-      }
-    },
-    [userInfo]
-  )
-
-  const logout = async () => {
-    await reset()
-    setUserInfo(null)
+export default class App extends Component {
+  state = {
+    userInfo: null,
+    verifying: false
   }
 
-  const resetOnError = () => {
-    if (Platform.OS == 'ios') {
-      DocumentBrowser.closeBrowser()
-    } else if (Platform.OS == 'android') {
-      NativeModules.AndroidDocumentBrowser.closeBrowser()
+  componentDidMount () {
+    this.retrieveUserSession()
+  }
+
+  setUserInfo(info, checkLicense) {
+    const userInfo = info && cloneDeep(info)
+    console.log('SETTING USER INFO:', userInfo)
+    this.setState({ userInfo }, checkLicense && this.checkUserLicense)
+  }
+
+  setVerifying(verifying) {
+    this.setState({ verifying })
+  }
+
+  showError (message) {
+    showAlert({
+      title: t('UH-OH!'),
+      message: t(message)
+    })
+  }
+
+  retrieveUserSession = async () => {
+    const userInfo = await getUserVerification()
+    this.setUserInfo(userInfo, true)
+  }
+
+  checkUserLicense = async () => {
+    const {
+      userInfo = {},
+      userInfo: { verified, validLicense }
+    } = this.state
+    const wasVerifiedAndValid = verified && validLicense
+    let isActive = false
+
+    // if we have a user check lets check their license
+    if (wasVerifiedAndValid) {
+      isActive = await checkStoredLicense(userInfo)
+    }
+
+    if (wasVerifiedAndValid && !isActive) {
+      // License was valid but is no longer active or valid
+      // alert user (and perhaps unset user session data)
+      this.showError('We found your user session but...')
+      this.handleLogout()
+      return
     }
   }
 
-  const recoverFromError = () => {
-    if (Platform.OS == 'ios') {
-      DocumentBrowser.openBrowser()
-    }
+  handleLogout () {
+    // unset user session
+    reset()
+    this.setUserInfo(null)
   }
 
-  const verifyLicense = async () => {
-    if (email == '') return
-    setVerifying(true)
+  handleLicenseVerification = async (email) => {
+    this.setVerifying(true)
     const [success, userInfo] = await checkForActiveLicense(email)
     if (success) {
-      Toast.show({
-        text: t('Success!'),
-        duration: 3000,
-        type: 'success'
-      })
-      sendVerificationEmail(email, userInfo.idToVerify, error => {
-        setVerifying(false)
-        if (error) {
-          Toast.show({
-            text: t('Error sending email! Try again or another email.'),
-            duration: 3000,
-            type: 'danger'
-          })
-          setUserInfo(null)
-        } else {
-          setUserInfo(userInfo)
-        }
-      })
+      this.setUserInfo(userInfo)
+      sendVerificationEmail(
+        email,
+        userInfo.idToVerify,
+        this.handleEmailConfirmation
+      )
     } else {
-      setVerifying(false)
-      Toast.show({
-        text: t(
-          `Error! That email didn't verify. Try again or another email.${
-            userInfo ? `\n${userInfo.message}` : ''
-          }`
-        ),
-        duration: 10000,
-        type: 'danger'
-      })
+      this.setVerifying(false)
+      this.showError(
+        "That email didn't verify. Try again or another email." +
+            '\n' + (userInfo && userInfo.message) || ''
+      )
     }
   }
 
-  const verifyCode = async () => {
-    setVerifying(true)
-    if (code == userInfo.idToVerify) {
-      Toast.show({
-        text: t('Success!'),
-        duration: 3000,
-        type: 'success'
-      })
-      const [verified, newUserInfo] = await verifyUser(userInfo)
+  handleEmailConfirmation = (error) => {
+    this.setVerifying(false)
+    if (error) {
+      this.showError('Error sending email! Check your email and Try again.')
+      this.setUserInfo(null)
+    }
+  }
+
+  handleCodeVerification = async (code) => {
+    const { userInfo } = this.state
+    const codeVerified = code == userInfo.idToVerify
+
+    this.setVerifying(true)
+    if (codeVerified) {
+      const [verified, verifiedUserInfo] = await verifyUser(userInfo)
       if (verified) {
-        // console.log('newUserInfo', newUserInfo)
-        setUserInfo({ ...newUserInfo }) // create a new object to force a re-render
+        this.setUserInfo(verifiedUserInfo)
       } else {
-        console.log('FAIL', newUserInfo)
-        setUserInfo(null)
-        // newUserInfo is the license verification response
-        let text = ''
-        if (
-          newUserInfo &&
-          newUserInfo.problem == 'no_activations_left' &&
-          !newUserInfo.hasActivationsLeft
-        ) {
-          // not valid because of number of activations
-          text = t(
-            'It looks like you have Plottr on the max number of devices already'
-          )
-        } else {
-          // invalid
-          text = t(
-            'There was an error activating your license key on this device'
-          )
-        }
-        Toast.show({
-          text: text,
-          duration: 5000,
-          type: 'danger'
-        })
+        // unverified
+        const unverifiedUser = verifiedUserInfo
+        const noMoreActivations =
+          unverifiedUser &&
+          unverifiedUser.problem == 'no_activations_left' &&
+          !unverifiedUser.hasActivationsLeft
+        const reasonForError = noMoreActivations
+          ? 'It looks like you have Plottr on the max number of devices already'
+          : 'There was an error activating your license key on this device'
+
+        console.log('FAIL', reasonForError, unverifiedUser)
+        this.setUserInfo(null)
+        this.showError(reason)
       }
     } else {
-      Toast.show({
-        text: t("Error! That code didn't verify. Try again."),
-        duration: 3000,
-        type: 'danger'
-      })
+      this.showError("That code didn't verify. Try again.")
     }
-    setVerifying(false)
+    this.setVerifying(false)
   }
 
-  const renderEnterEmail = () => {
-    return (
-      <Container>
-        <Content style={styles.content}>
-          <H1 style={styles.header}>{t('Welcome to Plottr!')}</H1>
-          <H2 style={styles.header}>{t("Let's verify your license")}</H2>
-          <Form style={styles.form}>
-            <Item inlineLabel last regular style={styles.label}>
-              <Label>{t('Email')}</Label>
-              <Input
-                key='email'
-                onChangeText={text => setEmail(text)}
-                autoCapitalize='none'
-                autoCompleteType='email'
-                keyboardType='email-address'
-              />
-            </Item>
-            <Button block primary onPress={verifyLicense}>
-              <Text>{t('Check')}</Text>
-              {verifying ? <Spinner color='white' /> : null}
-            </Button>
-          </Form>
-          <Text>{t('This will send you an email with a code')}</Text>
-          <View style={styles.centeredTextWrapper}>
-            <Text style={styles.centeredText}>
-              {t("Don't have a license? Go to our website to learn more")}
-            </Text>
-            <Button
-              transparent
-              large
-              onPress={() => Linking.openURL('https://getplottr.com')}
-              style={styles.ourWebsiteButton}>
-              <Text>getplottr.com</Text>
-            </Button>
-          </View>
-        </Content>
-      </Container>
+  handleAppReset = () => {
+    this.setState(
+      { userInfo: null, verifying: false },
+      this.retrieveUserSession
     )
   }
 
-  const renderVerification = () => {
+  render () {
+    const { userInfo, verifying } = this.state
+    const flexContainer = { flex: 1 }
+
+    console.log('userInfo', userInfo)
     return (
-      <Container>
-        <Content style={styles.content}>
-          <H1 style={styles.header}>{t('Welcome to Plottr!')}</H1>
-          <H2 style={styles.header}>
-            {t('Enter your verification code to activate this device')}
-          </H2>
-          <Form style={styles.form}>
-            <Item inlineLabel last regular style={styles.label}>
-              <Label>{t('Code')}</Label>
-              <Input
-                key='code'
-                onChangeText={text => setCode(text)}
-                autoCapitalize='none'
-                autoCompleteType='off'
-                keyboardType='number-pad'
-              />
-            </Item>
-            <Button block success onPress={verifyCode}>
-              <Text>{t('Verify')}</Text>
-              {verifying ? <Spinner color='white' /> : null}
-            </Button>
-          </Form>
-          <Text>{t('You should have received an email with a code')}</Text>
-          <View style={styles.centeredTextWrapper}>
-            <Text style={styles.centeredText}>
-              {t('Verifying with email: {email}', { email: userInfo.email })}
-            </Text>
-            <Button
-              transparent
-              large
-              onPress={() => setUserInfo(null)}
-              style={styles.ourWebsiteButton}>
-              <Text>{t('Use a different email')}</Text>
-            </Button>
-          </View>
-        </Content>
-      </Container>
+      <SafeAreaProvider>
+        <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+          <KeyboardAvoidingView
+            enabled
+            behavior={IS_ANDROID ? '' : 'padding'}
+            keyboardShouldPersistTaps={'always'}
+            style={flexContainer}>
+            <View style={flexContainer}>
+              <StatusBar barStyle='dark-content' />
+              <AppErrorBoundary reset={this.handleAppReset} recover={this.handleAppReset}>
+                <Main
+                  v2
+                  user={userInfo}
+                  loading={verifying}
+                  logout={this.handleLogout}
+                  verifyLicense={this.handleLicenseVerification}
+                  verifyCode={this.handleCodeVerification} />
+              </AppErrorBoundary>
+              <AlertDialog />
+            </View>
+          </KeyboardAvoidingView>
+        </TouchableWithoutFeedback>
+      </SafeAreaProvider>
     )
   }
-
-  // no userInfo -> enter email
-  // userInfo but not verified -> verification
-  // verified -> Main
-  const renderBody = () => {
-    if (__DEV__) {
-      // for validation bypass
-      return <Main v2={1} logout={logout} />
-    }
-    if (!userInfo) {
-      return renderEnterEmail()
-    }
-
-    if (userInfo && !userInfo.verified) {
-      return renderVerification()
-    }
-
-    if (userInfo && userInfo.verified) {
-      return <Main v2={userInfo.isV2} logout={logout} />
-    }
-  }
-
-  return (
-    <SafeAreaProvider>
-      <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
-        <KeyboardAvoidingView
-          enabled
-          behavior={IS_ANDROID ? '' : 'padding'}
-          keyboardShouldPersistTaps={'always'}
-          style={styles.container}>
-          <Root>
-            <StatusBar barStyle='dark-content' />
-            <AppErrorBoundary reset={resetOnError} recover={recoverFromError}>
-              {renderBody()}
-            </AppErrorBoundary>
-            <AlertDialog />
-          </Root>
-        </KeyboardAvoidingView>
-      </TouchableWithoutFeedback>
-    </SafeAreaProvider>
-  )
 }
-
-const styles = StyleSheet.create({
-  container: { flex: 1 },
-  scrollView: {
-    backgroundColor: 'white'
-  },
-  content: {
-    padding: 16
-  },
-  header: {
-    textAlign: 'center',
-    marginTop: 10,
-    marginBottom: 10
-  },
-  label: {
-    marginBottom: 16
-  },
-  form: {
-    marginVertical: 16
-  },
-  centeredTextWrapper: {
-    marginTop: 64,
-    justifyContent: 'center',
-    alignItems: 'center'
-  },
-  ourWebsiteButton: {
-    alignSelf: 'center'
-  },
-  centeredText: {
-    fontSize: 20
-  }
-})
-
-export default App
