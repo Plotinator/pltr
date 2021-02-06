99da9e3345d03deb0331d044f3bcb187e9c92271
Arian Hendricks <arian.hendricks@gmail.com>
2021-02-06 13:08:55 -0500

    Tag Details UI Improvement

--- src/components/phone/tags/TagDetails.js
+++ src/components/phone/tags/TagDetails.js
@@ -4,7 +4,7 @@ import PropTypes from 'react-proptypes'
 import { connect } from 'react-redux'
 import { bindActionCreators } from 'redux'
 import t from 'format-message'
-import { Input, Label, Item, H3, Button, Text } from 'native-base'
+import { Label, Item } from 'native-base'
 import { actions, selectors, initialState } from 'pltr/v2'
 import { StyleSheet, View, TouchableOpacity, Platform } from 'react-native'
 import SaveButton from '../../ui/SaveButton'
@@ -16,6 +16,9 @@ import {
   addLeaveListener,
   removeLeaveListener
 } from '../../../utils/Changes'
+import { Text, Input, Button } from '../../shared/common'
+import Colors from '../../../utils/Colors'
+import Metrics from '../../../utils/Metrics'
 
 const isIOS = Platform.OS == 'ios'
 
@@ -25,7 +28,9 @@ class TagDetails extends Component {
     const NewTag = cloneDeep(initialState.tag)
     const { route, tags } = this.props
     const { isNewTag, tag = {} } = route.params
-    const currentTag = isNewTag ? NewTag : tags.find(eTag => eTag.id == tag.id)
+    const currentTag = isNewTag
+      ? NewTag
+      : tags.find((eTag) => eTag.id == tag.id)
 
     this.state = {
       isNewTag,
@@ -36,23 +41,25 @@ class TagDetails extends Component {
     }
   }
 
-  componentDidMount () {
+  componentDidMount() {
     const { navigation } = this.props
     addLeaveListener(navigation, this.checkChanges)
     this.setSaveButton()
   }
 
-  componentDidUpdate () {
+  componentDidUpdate() {
     this.setSaveButton()
   }
 
   setSaveButton = () => {
     this.props.navigation.setOptions({
-      headerRight: () => <SaveButton changes={this.state.changes} onPress={this.saveChanges} />
+      headerRight: () => (
+        <SaveButton changes={this.state.changes} onPress={this.saveChanges} />
+      )
     })
   }
 
-  componentWillUnmount () {
+  componentWillUnmount() {
     const { navigation } = this.props
     removeLeaveListener(navigation, this.checkChanges)
   }
@@ -68,20 +75,20 @@ class TagDetails extends Component {
     if (!changes) return
     if (isNewTag) {
       this.props.actions.addTagWithValues(title, color)
-      this.props.navigation.setParams({isNewTag: false})
+      this.props.navigation.setParams({ isNewTag: false })
     } else {
       this.props.actions.editTag(id, title, color)
     }
-    this.setState({isNewTag: false, changes: false})
+    this.setState({ isNewTag: false, changes: false })
   }
 
   navigateToColorPicker = () => {
     const { id, colorFromRedux } = this.state
-    this.props.navigation.push('ColorPickerModal', {type: 'tag', id})
+    this.props.navigation.push('ColorPickerModal', { type: 'tag', id })
     // conscious trade-off here
     // this wipes out their typed color changes
     // but options are: 1. wipe out changes, 2. wipe out knowledge of changes (couldn't save), 3. don't show picked color
-    this.setState({changes: false, color: colorFromRedux})
+    this.setState({ changes: false, color: colorFromRedux })
   }
 
   toggleColorPicker = () => {
@@ -96,80 +103,110 @@ class TagDetails extends Component {
     this.setState({ showColorPicker: false })
   }
 
-  handleSelectColor = (color) => this.setState({ color, changes: true, showColorPicker: false })
+  handleSelectColor = (color) =>
+    this.setState({ color, changes: true, showColorPicker: false })
 
-  renderColorPicker () {
+  renderColorPicker() {
     const { showColorPicker, color } = this.state
-    return showColorPicker ? <ColorPickerModal expressMode currentColor={color} onClose={this.hideColorPicker} chooseColor={this.handleSelectColor} /> : null
+    return showColorPicker ? (
+      <ColorPickerModal
+        key='picker'
+        expressMode
+        currentColor={color}
+        onClose={this.hideColorPicker}
+        chooseColor={this.handleSelectColor}
+      />
+    ) : null
   }
 
-  render () {
+  render() {
     const { title, color } = this.state
     const colorObj = tinycolor(color || 'black')
-    const backgroundColor = {backgroundColor: colorObj.toHexString()}
-    return <DetailsScrollView>
-      <Item inlineLabel last regular style={styles.label}>
-        <Label style={styles.NBlabel}>{t('Title')}:</Label>
-        <Input
-          style={styles.input}
-          value={title}
-          onChangeText={text => this.setState({title: text, changes: true})}
-          autoCapitalize='sentences'
-        />
-      </Item>
-      <Item inlineLabel last regular style={styles.label}>
-        <Label style={styles.NBlabel}>{t('Color')}:</Label>
-        <Input
-          style={styles.input}
-          value={color}
-          onChangeText={text => this.setState({color: text, changes: true})}
-        />
-      </Item>
-      <View style={styles.colorWrapper}>
-        <H3>{t('Current Color')}</H3>
-        <TouchableOpacity onPress={this.toggleColorPicker}><View style={[styles.colorSwatch, backgroundColor]} /></TouchableOpacity>
-        <Button bordered light style={styles.button} onPress={this.toggleColorPicker}><Text style={styles.buttonText}>{t('Choose Color')}</Text></Button>
-      </View>
-      {this.renderColorPicker()}
-    </DetailsScrollView>
+    const backgroundColor = { backgroundColor: colorObj.toHexString() }
+    return ([
+      <DetailsScrollView style={styles.container} key='main'>
+        <View style={styles.label}>
+          <Input
+            inset
+            label={`${t('Title')}:`}
+            value={title}
+            onChangeText={(text) =>
+              this.setState({ title: text, changes: true })
+            }
+            autoCapitalize='sentences'
+          />
+        </View>
+        <View style={styles.label}>
+          <Input
+            inset
+            label={`${t('Color')}:`}
+            value={color}
+            onChangeText={(text) =>
+              this.setState({ color: text, changes: true })
+            }
+          />
+        </View>
+        <View style={styles.colorWrapper}>
+          <Text fontSize='h4' fontStyle='bold'>
+            {t('Current Color')}
+          </Text>
+          <TouchableOpacity onPress={this.toggleColorPicker}>
+            <View style={[styles.colorSwatch, backgroundColor]} />
+          </TouchableOpacity>
+          <Button
+            tight
+            bordered
+            // textColor='darkGray'
+            buttonColor='gray'
+            style={styles.button}
+            onPress={this.toggleColorPicker}>
+            {t('Choose Color')}
+          </Button>
+        </View>
+      </DetailsScrollView>,
+      this.renderColorPicker()
+    ])
   }
 }
 
 const styles = StyleSheet.create({
+  container: {
+    flex: 1
+  },
   label: {
-    paddingLeft: 15,
-    marginBottom: 16,
+    paddingHorizontal: Metrics.baseMargin,
+    marginVertical: Metrics.baseMargin
   },
   NBlabel: {
-    paddingRight: 0,
+    paddingRight: 0
   },
   input: {
     marginTop: isIOS ? -2 : 1
   },
   afterList: {
-    marginTop: 16,
+    marginTop: 16
   },
   currentColor: {
     height: 50,
-    width: 50,
+    width: 50
   },
   colorWrapper: {
     marginTop: 8,
-    alignItems: 'center',
+    alignItems: 'center'
   },
   colorSwatch: {
     width: 60,
     height: 50,
     margin: 10,
     borderBottomRightRadius: 10,
-    borderTopLeftRadius: 10,
+    borderTopLeftRadius: 10
   },
   button: {
     alignSelf: 'center',
-    marginTop: 8,
+    marginTop: 8
   },
   buttonText: {
-    color: 'black',
+    color: 'black'
   }
 })
 
@@ -180,25 +217,22 @@ TagDetails.propTypes = {
   actions: PropTypes.object.isRequired,
   ui: PropTypes.object.isRequired,
   navigation: PropTypes.object.isRequired,
-  route: PropTypes.object.isRequired,
+  route: PropTypes.object.isRequired
 }
 
-function mapStateToProps (state) {
+function mapStateToProps(state) {
   return {
     tags: selectors.sortedTagsSelector(state),
     characters: selectors.charactersSortedAtoZSelector(state),
     places: selectors.placesSortedAtoZSelector(state),
-    ui: state.ui,
+    ui: state.ui
   }
 }
 
-function mapDispatchToProps (dispatch) {
+function mapDispatchToProps(dispatch) {
   return {
     actions: bindActionCreators(actions.tagActions, dispatch)
   }
 }
 
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(TagDetails)
+export default connect(mapStateToProps, mapDispatchToProps)(TagDetails)
