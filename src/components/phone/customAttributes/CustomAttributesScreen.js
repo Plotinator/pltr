8f3c167eacdc1c38dbab53594771b304f8ca549e
Arian Hendricks <arian.hendricks@gmail.com>
2021-02-06 17:10:39 -0500

    Custom Attributes Input bug fix + UI Improvements

--- src/components/phone/customAttributes/CustomAttributesScreen.js
+++ src/components/phone/customAttributes/CustomAttributesScreen.js
@@ -3,15 +3,40 @@ import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
 import { bindActionCreators } from 'redux'
 import { actions, selectors } from 'pltr/v2'
-import { StyleSheet, View, FlatList, TouchableOpacity, LayoutAnimation, Platform, UIManager } from 'react-native'
-import { Icon, H1, Item, Input, Button, Label, Container, Content, Text, ListItem, Left, Switch, Body, Right } from 'native-base'
+import {
+  StyleSheet,
+  View,
+  LayoutAnimation,
+  Platform,
+  UIManager
+} from 'react-native'
+import {
+  Icon,
+  H1,
+  Item,
+  Label,
+  Container,
+  Content,
+  ListItem,
+  Left,
+  Switch,
+  Body,
+  Right
+} from 'native-base'
 import { SwipeListView } from 'react-native-swipe-list-view'
 import t from 'format-message'
 import RenameButton from '../../ui/RenameButton'
 import TrashButton from '../../ui/TrashButton'
 import { askToDelete } from '../../../utils/delete'
+import { Text, Button, FloatingInput } from '../../shared/common'
+import Fonts from '../../../fonts'
+import Metrics from '../../../utils/Metrics'
+import Colors from '../../../utils/Colors'
 
-if (Platform.OS === "android" && UIManager.setLayoutAnimationEnabledExperimental) {
+if (
+  Platform.OS === 'android' &&
+  UIManager.setLayoutAnimationEnabledExperimental
+) {
   UIManager.setLayoutAnimationEnabledExperimental(true)
 }
 
@@ -23,6 +48,8 @@ class CustomAttributesScreen extends Component {
     this.state = { type, text: '' }
   }
 
+  setText = text => this.setState({ text })
+
   add = () => {
     const { text, type } = this.state
     const { actions, restrictedValues } = this.props
@@ -30,12 +57,12 @@ class CustomAttributesScreen extends Component {
 
     LayoutAnimation.configureNext(LayoutAnimation.Presets.easeInEaseOut)
     if (type == 'characters') {
-      actions.addCharacterAttr({name: text, type: 'text'})
+      actions.addCharacterAttr({ name: text, type: 'text' })
     }
     if (type == 'places') {
-      actions.addPlaceAttr({name: text, type: 'text'})
+      actions.addPlaceAttr({ name: text, type: 'text' })
     }
-    this.setState({text: ''})
+    this.setState({ text: '' })
   }
 
   deleteAttr = (attr) => {
@@ -56,10 +83,10 @@ class CustomAttributesScreen extends Component {
     if (text == attr.name) return
 
     if (type == 'characters') {
-      actions.editCharacterAttr(index, attr, {...attr, name: text})
+      actions.editCharacterAttr(index, attr, { ...attr, name: text })
     }
     if (type == 'places') {
-      actions.editPlaceAttr(index, attr, {...attr, name: text})
+      actions.editPlaceAttr(index, attr, { ...attr, name: text })
     }
   }
 
@@ -69,72 +96,107 @@ class CustomAttributesScreen extends Component {
     const newType = val ? 'paragraph' : 'text'
 
     if (type == 'characters') {
-      actions.editCharacterAttr(index, attr, {...attr, type: newType})
+      actions.editCharacterAttr(index, attr, { ...attr, type: newType })
     }
     if (type == 'places') {
-      actions.editPlaceAttr(index, attr, {...attr, type: newType})
+      actions.editPlaceAttr(index, attr, { ...attr, type: newType })
     }
   }
 
   renderItem = ({ item, index }) => {
-    const canChange = this.props.customAttributesThatCanChange.includes(item.name)
+    const canChange = this.props.customAttributesThatCanChange.includes(
+      item.name
+    )
     if (canChange) {
-      return <ListItem noIndent style={styles.row}>
-        <Left>
-          <Text>{item.name}</Text>
-        </Left>
-        <Body>
-          <Text>{t('Paragraph?')}</Text>
-        </Body>
-        <Right>
-          <Switch value={item.type == 'paragraph'} onValueChange={val => this.toggleAttr(val, item, index)}/>
-        </Right>
-      </ListItem>
+      return (
+        <ListItem noIndent style={styles.row}>
+          <Left style={styles.left}>
+            <Text fontStyle='semiBold'>{item.name}</Text>
+          </Left>
+          <Body>
+            <Text fontSize='small'>{t('Paragraph?')}</Text>
+          </Body>
+          <Right style={styles.right}>
+            <Switch
+              value={item.type == 'paragraph'}
+              onValueChange={(val) => this.toggleAttr(val, item, index)}
+            />
+          </Right>
+        </ListItem>
+      )
     } else {
-      return <ListItem noIndent style={styles.row}>
-        <Left>
-          <Text>{item.name}</Text>
-        </Left>
-        <Body>
-          <Text note>{t('Paragraph')}</Text>
-        </Body>
-      </ListItem>
+      return (
+        <ListItem noIndent style={styles.row}>
+          <Left style={styles.left}>
+            <Text>{item.name}</Text>
+          </Left>
+          <Body>
+            <Text fontSize='small'>{t('Paragraph')}</Text>
+          </Body>
+        </ListItem>
+      )
     }
   }
 
   render () {
-    return <Container>
-      <Content padder>
-        <View style={styles.container}>
-          <Item floatingLabel style={styles.newAttr}>
-            <Label>{t('New Custom Attribute Title')}</Label>
-            <Input value={this.state.text} onChangeText={text => this.setState({text})} placeholder={t('Enter Custom Attribute Title')}/>
-          </Item>
-          <Button full success disabled={!this.state.text} onPress={this.add} style={styles.button}><Text>{t('Add')}</Text></Button>
-          <SwipeListView
-            data={this.props.customAttributes}
-            keyExtractor={(item) => item.name}
-            renderItem={this.renderItem}
-            renderHiddenItem={ (data, rowMap) => <TrashButton onPress={() => this.deleteAttr(data.item)} />}
-            leftOpenValue={75}
+    const { text } = this.state
+    return (
+      <View style={styles.container}>
+        <View style={styles.subContent}>
+          <FloatingInput
+            center
+            value={text}
+            onChangeText={this.setText}
+            placeholder=''
+            inputStyle={styles.input}
+            labelText={t('Enter Custom Attribute Title')}
           />
+          <Button
+            tight
+            disabled={!text}
+            onPress={this.add}
+            style={styles.button}>
+            {t('Add')}
+          </Button>
         </View>
-      </Content>
-    </Container>
+        <SwipeListView
+          data={this.props.customAttributes}
+          keyExtractor={(item) => item.name}
+          renderItem={this.renderItem}
+          renderHiddenItem={({ item }, rowMap) => (
+            <TrashButton data={item} iconStyle={styles.icon} onPress={this.deleteAttr} />
+          )}
+          leftOpenValue={75}
+        />
+      </View>
+    )
   }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    paddingVertical: 8,
+    backgroundColor: Colors.white,
+    paddingVertical: Metrics.baseMargin,
+  },
+  subContent: {
+    paddingHorizontal: Metrics.doubleBaseMargin,
+    paddingBottom: Metrics.baseMargin,
+    borderBottomWidth: 0.5,
+    borderBottomColor: Colors.borderGray
   },
   h1: {
     textAlign: 'center',
-    marginVertical: 20,
+    marginVertical: 20
   },
   row: {
-    backgroundColor: 'white',
+    backgroundColor: 'white'
+  },
+  left: {
+    paddingLeft: Metrics.baseMargin
+  },
+  right: {
+    paddingRight: Metrics.baseMargin
   },
   hiddenRow: {
     flex: 1,
@@ -143,18 +205,31 @@ const styles = StyleSheet.create({
     alignItems: 'center'
   },
   button: {
-    marginVertical: 8,
+    marginTop: Metrics.doubleBaseMargin,
+    marginVertical: Metrics.baseMargin
   },
   newAttr: {
-    marginBottom: 8,
+    marginBottom: 8
   },
+  labelText: {
+    ...Fonts.style.regular,
+    height: 'auto',
+    paddingTop: Metrics.baseMargin * 0.8,
+    paddingRight: 0
+  },
+  icon: {
+    color: 'white'
+  },
+  input: {
+    color: Colors.darkGray
+  }
 })
 
 CustomAttributesScreen.propTypes = {
   customAttributes: PropTypes.array.isRequired,
   customAttributesThatCanChange: PropTypes.array,
   restrictedValues: PropTypes.array,
-  actions: PropTypes.object.isRequired,
+  actions: PropTypes.object.isRequired
 }
 
 function mapStateToProps (state, ownProps) {
@@ -165,7 +240,9 @@ function mapStateToProps (state, ownProps) {
   let restrictedValues = []
   if (type == 'characters') {
     canChange = selectors.characterCustomAttributesThatCanChangeSelector(state)
-    restrictedValues = selectors.characterCustomAttributesRestrictedValues(state)
+    restrictedValues = selectors.characterCustomAttributesRestrictedValues(
+      state
+    )
   } else {
     canChange = selectors.placeCustomAttributesThatCanChangeSelector(state)
     restrictedValues = selectors.placeCustomAttributesRestrictedValues(state)
@@ -174,13 +251,13 @@ function mapStateToProps (state, ownProps) {
   return {
     customAttributes: state.customAttributes[type],
     customAttributesThatCanChange: canChange,
-    restrictedValues: restrictedValues,
+    restrictedValues: restrictedValues
   }
 }
 
 function mapDispatchToProps (dispatch) {
   return {
-    actions: bindActionCreators(actions.customAttributeActions, dispatch),
+    actions: bindActionCreators(actions.customAttributeActions, dispatch)
   }
 }
 
