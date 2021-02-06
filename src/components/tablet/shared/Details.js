6e9951b99723f251c2c2d08b193476cd20be20ae
Arian Hendricks <arian.hendricks@gmail.com>
2021-02-06 13:06:23 -0500

    Fixed scrolling bug

--- src/components/tablet/shared/Details.js
+++ src/components/tablet/shared/Details.js
@@ -1,75 +1,82 @@
 import React from 'react'
 import { View, Form, Label, Input } from 'native-base'
-import { StyleSheet, ScrollView } from 'react-native'
+import { StyleSheet, ScrollView, TouchableWithoutFeedback } from 'react-native'
 
 export function DetailsWrapper (props) {
-
-  return <View style={styles.detailsWrapper}>
-    <View style={styles.detailsInner}>
-      <View style={styles.contentWrapper}>
-        <View style={styles.content}>
-          <Form style={styles.form}>
-            { props.children }
-          </Form>
+  return (
+    <View style={styles.detailsWrapper}>
+      <View style={styles.detailsInner}>
+        <View style={styles.contentWrapper}>
+          <View style={styles.content}>
+            <Form style={styles.form}>{props.children}</Form>
+          </View>
         </View>
       </View>
     </View>
-  </View>
+  )
 }
 
 export function DetailsLeft (props) {
-  return <View style={styles.formLeft}><ScrollView contentContainerStyle={props.contentContainerStyle}>{ props.children }</ScrollView></View>
+  return (
+    <View style={styles.formLeft}>
+      <ScrollView contentContainerStyle={props.contentContainerStyle}>
+        <TouchableWithoutFeedback>
+          <View>{props.children}</View>
+        </TouchableWithoutFeedback>
+      </ScrollView>
+    </View>
+  )
 }
 
 export function DetailsRight (props) {
-  return <View style={styles.formRight}>
-    <View style={styles.formRightInner}>
-      { props.children }
+  return (
+    <View style={styles.formRight}>
+      <View style={styles.formRightInner}>{props.children}</View>
     </View>
-  </View>
+  )
 }
 
 const styles = StyleSheet.create({
   detailsWrapper: {
     padding: 16,
-    flex: 1,
+    flex: 1
   },
   detailsInner: {
     flex: 1,
     backgroundColor: 'white',
     borderRadius: 20,
-    shadowColor: "#000",
+    shadowColor: '#000',
     shadowOffset: {
       width: 0,
-      height: 5,
+      height: 5
     },
     shadowOpacity: 0.34,
-    shadowRadius: 6.27,
+    shadowRadius: 6.27
   },
   contentWrapper: {
-    flex: 1,
+    flex: 1
   },
   content: {
-    flex: 1,
+    flex: 1
   },
   form: {
     flex: 1,
-    flexDirection: 'row',
+    flexDirection: 'row'
   },
   formRight: {
     flex: 0.25,
     paddingLeft: 8,
     borderTopRightRadius: 20,
     borderBottomRightRadius: 20,
-    backgroundColor: 'hsl(210, 36%, 96%)', //gray-9
+    backgroundColor: 'hsl(210, 36%, 96%)' // gray-9
   },
   formRightInner: {
     flex: 1,
     flexDirection: 'column',
-    justifyContent: 'space-between',
+    justifyContent: 'space-between'
   },
   formLeft: {
     flex: 0.75,
-    paddingHorizontal: 16,
-  },
+    paddingHorizontal: 16
+  }
 })
