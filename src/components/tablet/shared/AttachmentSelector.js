14401bcbada7e9e88c77623070190b82017e9f1d
Arian Hendricks <arian.hendricks@gmail.com>
2021-02-25 13:35:11 -0500

    Fixed Attachment Selector issue with displaying Books + minor UI tweaks

--- src/components/tablet/shared/AttachmentSelector.js
+++ src/components/tablet/shared/AttachmentSelector.js
@@ -3,18 +3,38 @@ import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
 import { bindActionCreators } from 'redux'
 import { actions, selectors } from 'pltr/v2'
-import { StyleSheet, FlatList } from 'react-native'
-import { Icon, H1, H3, ListItem, CheckBox, Body, Text, View, Badge, Left, Right } from 'native-base'
+import { StyleSheet, ScrollView, FlatList } from 'react-native'
+import {
+  Icon,
+  ListItem,
+  CheckBox,
+  Body,
+  View,
+  Badge,
+  Left,
+  Right
+} from 'native-base'
 import Popover from 'react-native-popover-view'
 import t from 'format-message'
 import { attachmentItemText } from '../../../utils/attachment_titles'
+import { ShellButton, Text } from '../../shared/common'
+import Colors from '../../../utils/Colors'
+import Fonts from '../../../fonts'
+import Metrics from '../../../utils/Metrics'
 
 class AttachmentSelector extends Component {
-  state = { selected: []}
+  state = { selected: [] }
   static getDerivedStateFromProps (props, state) {
     const { type, item, itemType } = props
-    const connectedItem = props[`${itemType}s`].find(things => things.id == item.id)
-    return { type, item: connectedItem, itemType, selected: connectedItem[type] }
+    const connectedItem = props[`${itemType}s`].find(
+      (things) => things.id == item.id
+    )
+    return {
+      type,
+      item: connectedItem,
+      itemType,
+      selected: connectedItem[type]
+    }
   }
 
   addIt = (id) => {
@@ -54,78 +74,139 @@ class AttachmentSelector extends Component {
 
   toggleItem = (id) => {
     let ids = this.state.selected
-    if (ids.includes(id)) {
+    if (ids.includes (id)) {
       this.removeIt(id)
     } else {
       this.addIt(id)
     }
   }
 
-  renderItem = ({ item }) => {
+  renderItem = (key) => {
     const { type, selected } = this.state
-    let color = {}
-    if (type == 'tags') color = {backgroundColor: item.color}
-    let defaultTitle = `New ${type.substr(0,1).toUpperCase()}${type.substr(1, type.length - 2)}`
-    return <ListItem noIndent button key={`item-${item.id}`} style={styles.listItem} onPress={() => this.toggleItem(item.id)}>
-      <CheckBox checked={selected.includes(item.id)} onPress={() => this.toggleItem(item.id)}/>
-      <Body>
-        <View style={styles.rowView}>
-          <H3 style={styles.title}>{item.name || item.title || defaultTitle}</H3>
-          {type == 'tags' ? <Badge style={[styles.badge, color]}><Text>{item.color}</Text></Badge> : null}
-        </View>
-      </Body>
-    </ListItem>
+    const dataSet = this.props[type]
+    const item = dataSet[key]
+    if (item && item.id) {
+      let color = {}
+      if (type == 'tags') color = { backgroundColor: item.color }
+      let defaultTitle = `New ${type.substr(0, 1).toUpperCase()}${type.substr(
+        1,
+        type.length - 2
+      )}`
+      return (
+        <ListItem
+          noIndent
+          button
+          key={`item-${item.id}`}
+          style={styles.listItem}
+          onPress={() => this.toggleItem(item.id)}>
+          <CheckBox
+            checked={selected.includes(item.id)}
+            onPress={() => this.toggleItem(item.id)}
+          />
+          <Body>
+            <View style={styles.rowView}>
+              <Text fontStyle='semiBold' fontSize='small' style={styles.title}>
+                {item.name || item.title || defaultTitle}
+              </Text>
+              {type == 'tags' ? (
+                <Badge style={[styles.badge, color]}></Badge>
+              ) : null}
+            </View>
+          </Body>
+        </ListItem>
+      )
+    }
   }
 
   render () {
     const { item, type } = this.props
-    return <Popover
-      from={
-        <ListItem>
-          <Left>
-            <Badge info style={styles.listBadge}><Text>{item[type].length}</Text></Badge>
-            <Text>{attachmentItemText(type)}</Text>
-          </Left>
-          <Right>
-            <Icon type='FontAwesome5' name='chevron-right'/>
-          </Right>
-        </ListItem>
-      }
-    >
-      <FlatList
-        data={this.props[this.state.type]}
-        ListEmptyComponent={<H1 style={styles.h1}>{t('None to choose')}</H1>}
-        extraData={{selected: this.state.selected}}
-        keyExtractor={(item) => item.id.toString()}
-        renderItem={this.renderItem}
-      />
-    </Popover>
+    const { type: stateType } = this.state
+    const dataSet = this.props[stateType] || []
+    const dataKeys = Object.keys(dataSet)
+    return (
+      <Popover
+        from={
+          <ShellButton style={styles.listRow}>
+            <View style={styles.left}>
+              <Badge info style={styles.listBadge}>
+                <Text style={styles.badgeNumber}>{item[type].length}</Text>
+              </Badge>
+              <Text fontStyle='semiBold' fontSize='h7'>
+                {attachmentItemText(type)}
+              </Text>
+            </View>
+            <View style={styles.right}>
+              <Icon
+                type='FontAwesome5'
+                name='chevron-right'
+                style={styles.caret}
+              />
+            </View>
+          </ShellButton>
+        }>
+        <ScrollView>
+          {dataKeys.map(this.renderItem)}
+          {dataKeys.length == 0 && (
+            <Text fontStyle='semiBold' fontSize='standard' style={styles.h1}>
+              {t('None to choose')}
+            </Text>
+          )}
+        </ScrollView>
+      </Popover>
+    )
   }
 }
 
 const styles = StyleSheet.create({
   h1: {
     textAlign: 'center',
-    marginVertical: 20,
+    marginVertical: 20
   },
   title: {
-    paddingVertical: 4,
+    paddingVertical: 4
   },
   rowView: {
     flexDirection: 'row',
     alignItems: 'center',
-    paddingLeft: 10,
+    paddingLeft: 10
   },
   badge: {
+    width: 50,
     marginTop: 5,
-    marginLeft: 20,
+    marginLeft: 20
   },
   listBadge: {
-    marginRight: 8,
+    height: 30,
+    width: 30,
+    marginRight: Metrics.baseMargin / 1.5,
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  badgeNumber: {
+    fontSize: Fonts.size.h7,
+    color: 'white'
   },
   listItem: {
-    width: 500,
+    width: 500
   },
+  listRow: {
+    flexDirection: 'row',
+    paddingVertical: Metrics.baseMargin,
+    paddingHorizontal: Metrics.baseMargin / 3
+  },
+  left: {
+    flex: 1,
+    flexDirection: 'row',
+    alignItems: 'center'
+  },
+  right: {
+    alignItems: 'flex-end',
+    justifyContent: 'center'
+  },
+  caret: {
+    color: Colors.lightGray,
+    fontSize: Fonts.size.small
+  }
 })
 
 AttachmentSelector.propTypes = {
@@ -135,19 +216,30 @@ AttachmentSelector.propTypes = {
   books: PropTypes.object.isRequired,
   cards: PropTypes.array.isRequired,
   notes: PropTypes.array.isRequired,
-  actions: PropTypes.object.isRequired,
+  actions: PropTypes.object.isRequired
 }
 
 function mapStateToProps (state) {
   console.log('BOOKS', state.books)
+  console.log('TAGS', state.tags)
+  console.log('PLACES', state.places)
+  console.log('CHARACTERS', state.characters)
+  const { books = [] } = state
+  const bookIds = {}
+  Object.keys(books).map((key, index) => {
+    // if (key !== 'allIds') {
+    bookIds[index] = { ...books[key] }
+    // }
+  })
+  console.log('bookIds', bookIds)
   return {
     characters: selectors.charactersSortedAtoZSelector(state),
     places: selectors.placesSortedAtoZSelector(state),
     tags: selectors.sortedTagsSelector(state),
     books: state.books,
-    // bookIds: state.books,
+    bookIds,
     cards: state.cards,
-    notes: state.notes,
+    notes: state.notes
   }
 }
 
@@ -155,19 +247,16 @@ function mapDispatchToProps (dispatch, ownProps) {
   const { itemType } = ownProps
   switch (itemType) {
     case 'card':
-      return {actions: bindActionCreators(actions.card, dispatch)}
+      return { actions: bindActionCreators(actions.card, dispatch) }
     case 'note':
-      return {actions: bindActionCreators(actions.note, dispatch)}
+      return { actions: bindActionCreators(actions.note, dispatch) }
     case 'character':
-      return {actions: bindActionCreators(actions.character, dispatch)}
+      return { actions: bindActionCreators(actions.character, dispatch) }
     case 'place':
-      return {actions: bindActionCreators(actions.place, dispatch)}
+      return { actions: bindActionCreators(actions.place, dispatch) }
     default:
       break
   }
 }
 
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(AttachmentSelector)
+export default connect(mapStateToProps, mapDispatchToProps)(AttachmentSelector)
