ff3e64855aae20f8a7b386c557a377385a5d0f9d
Arian Hendricks <arian.hendricks@gmail.com>
2021-04-04 17:22:18 -0500

    first Paragraph margin fix

--- src/components/shared/common/RichEditor.js
+++ src/components/shared/common/RichEditor.js
@@ -62,7 +62,7 @@ export default class RichTextEditor extends Component {
     const html = initialHTMLText || initialValue
     const initialText = typeof html == 'object' ? SlateToHTML(html) : html
     const contentCSSText = `font-family: "Open Sans" !important; font-size: 18px; padding: 5px 15px 15px;`
-    const cssText = `@import url('https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,400;0,700;1,400;1,700&display=swap');`
+    const cssText = `@import url('https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,400;0,700;1,400;1,700&display=swap'); p { margin-top: 0 !important; }`
     return (
       <View style={containerStyles}>
         <RichEditor
