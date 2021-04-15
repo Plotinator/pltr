12e14efcaad4310b7a4e0958ff648092e2be7e3d
Arian Hendricks <arian.hendricks@gmail.com>
2021-04-15 07:20:27 -0500

    Tablet Input adjustment

--- src/components/shared/common/InputStyles.js
+++ src/components/shared/common/InputStyles.js
@@ -2,7 +2,9 @@ import { ScaledSheet } from 'react-native-size-matters'
 import Metrics from '../../../utils/Metrics'
 import Fonts from '../../../fonts'
 import Colors from '../../../utils/Colors'
+import { isTablet } from 'react-native-device-info'
 
+const onTablet = isTablet()
 const {
   white,
   textGray,
@@ -90,7 +92,7 @@ export default ScaledSheet.create({
     marginBottom: 0
   },
   labelDown: {
-    marginTop: 26,
-    marginBottom: -26
+    marginTop: '26@vs',
+    marginBottom: '-26@vs'
   }
 })
