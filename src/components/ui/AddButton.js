f00ff865fc4207d71d5486f4fc84d35164c0eea6
Edward Steere <edward.steere@gmail.com>
2021-05-08 12:46:11 +0200

    Revert "Revert"
    This reverts commit 1d79f1df6da4d5bf0a6a657b53fb822e2c75d317.

--- src/components/ui/AddButton.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import { Button, Icon } from 'native-base'
-
-export default function AddButton (props) {
-  const noop = () => {}
-  return <Button transparent onPress={props.onPress ?? noop} style={props.buttonStyle ?? {}}>
-    <Icon type='FontAwesome5' name='plus' style={[{color: '#ff7f32'}, props.iconStyle]}/>
-  </Button>
-}
