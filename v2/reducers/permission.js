7e91b72116757ddf3f6e05fb69f1d100cc3aa81b
Edward Steere <edward.steere@gmail.com>
2021-06-21 15:37:44 +0200

    Reformat permission reducer

--- v2/reducers/permission.js
+++ v2/reducers/permission.js
@@ -2,15 +2,17 @@ import { SET_PERMISSION } from '../constants/ActionTypes'
 
 const INITIAL_STATE = { permission: 'owner' }
 
-const permissionReducer = (dataRepairers) => (state = INITIAL_STATE, action) => {
-  switch (action.type) {
-    case SET_PERMISSION:
-      return {
-        permission: action.permission,
-      }
-    default:
-      return state
+const permissionReducer =
+  (dataRepairers) =>
+  (state = INITIAL_STATE, action) => {
+    switch (action.type) {
+      case SET_PERMISSION:
+        return {
+          permission: action.permission,
+        }
+      default:
+        return state
+    }
   }
-}
 
 export default permissionReducer
